995910bebe7238eb9f4361f9217767f94504bb29
tushar-tyagi01 <tushartyagi2110@gmal.com>
2026-05-25 09:20:38 +0530

    fix productions cors

--- index.js
+++ index.js
@@ -10,14 +10,18 @@ dotenv.config();
 
 const app = express();
 
-if (process.env.NODE_ENV !== 'test') {
-  app.use(morgan('combined'))
+if (process.env.NODE_ENV !== "test") {
+  app.use(morgan("combined"));
 }
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173", "http://localhost:5174"],
-    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://staffmanagement-frontend.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200,
